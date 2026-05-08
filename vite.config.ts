563ea8a9505fa25dbb4529c22290e651075ab237
sbonholo <sbonholo@gmail.com>
2026-05-07 19:17:32 -0500

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+ÍÑÔÓÖØØÅÍlwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      '@': path.resolve('__dirname', 'src'),
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3000',
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
