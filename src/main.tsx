02f2251d4d3a631da4462c0a6b0a30a0e9034486
sbonholo <sbonholo@gmail.com>
2026-05-07 19:20:50 -0500

    Add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/firebase-messaging-sw.js', {
+      scope: '/',
+    });
+  });
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
