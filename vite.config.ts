5d36c5686c2aca2726a0a60e1b8f0f4876d7ec3b
sbonholo <sbonholo@gmail.com>
2026-05-10 11:07:44 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-ÍÑÔÓÖØØÅÍlwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
-      '@': path.resolve('__dirname', 'src'),
+      alias: {
+        '@': path.resolve('__dirname', 'src'),
+      },
     },
     server: {
       proxy: {
